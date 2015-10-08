dd0cef1bfb150a1c997bf154fa97a36c2edb9c28
Rudolf Hersén <rudolf@qwaya.com>
2015-10-08 22:13:52 +0200

    jshint

--- webgl.js
+++ webgl.js
@@ -1,8 +1,11 @@
+/* global alert */
+/* exported webgl */
+
 function webgl() {
-    const PERIOD = 20000;
-    const PI2 = 2 * Math.PI;
-    var canvas = document.getElementById("webgl");
-    var gl = canvas.getContext("webgl");
+    var PERIOD = 20000;
+    var PI2 = 2 * Math.PI;
+    var canvas = document.getElementById('webgl');
+    var gl = canvas.getContext('webgl');
     var vertexArray = new Array(6);
     var vertices = new Float32Array(vertexArray);
     var program = setupProgram();
@@ -54,19 +57,22 @@ function webgl() {
     function createTextureCoordinateBuffer() {
         var texCoordBuf = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuf);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0, 0, 1, 0, 0.5, 1]), gl.STATIC_DRAW);
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
+            new Float32Array([0, 0, 1, 0, 0.5, 1]),
+            gl.STATIC_DRAW);
         return texCoordBuf;
     }
 
     function bind() {
-        gl.uniform4fv(gl.getUniformLocation(program, "color"), [0, 0, 1, 1]);
+        gl.uniform4fv(gl.getUniformLocation(program, 'color'), [0, 0, 1, 1]);
 
-        var loc = gl.getAttribLocation(program, "pos");
+        var loc = gl.getAttribLocation(program, 'pos');
         gl.enableVertexAttribArray(loc);
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuf);
         gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0);
 
-        loc = gl.getAttribLocation(program, "txc");
+        loc = gl.getAttribLocation(program, 'txc');
         gl.enableVertexAttribArray(loc);
         gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuf);
         gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0);
@@ -80,7 +86,7 @@ function webgl() {
 
         function updateVertices(millis) {
             var d = PI2 / 3;
-            var angle = (millis % PERIOD) * PI2 / PERIOD;
+            var angle = millis % PERIOD * PI2 / PERIOD;
 
             for (var i = 0; i < 3; i++) {
                 vertexArray[i * 2] = Math.cos(angle);
