f61eac1472e8193b96e60ebb0929c0202d16b178
Rudolf Hersén <rudolf@qwaya.com>
2015-10-25 21:45:29 +0100

    Move calculations from js to vertex shader

--- webgl.js
+++ webgl.js
@@ -5,8 +5,8 @@ function webgl() {
     var PI2 = 2 * Math.PI
     var canvas = document.getElementById('webgl')
     var gl = canvas.getContext('webgl')
-    var n = 8;
-    var vertices = new Float32Array(n * 2)
+    var n = 5;
+    var angles = new Float32Array(n)
 
     bind()
 
@@ -24,8 +24,8 @@ function webgl() {
         var r = gl.createProgram()
 
         gl.attachShader(r, createShader(gl.VERTEX_SHADER, [
-            'attribute vec2 pos',
-            'void main() { gl_Position = vec4(pos, 0, 1)',
+            'attribute float angle',
+            'void main() { gl_Position = vec4(cos(angle), sin(angle), 0, 1)',
             '}'
         ].join(';')))
 
@@ -49,27 +49,24 @@ function webgl() {
     }
 
     function bind() {
-        var loc = gl.getAttribLocation(setupProgram(), 'pos')
+        var loc = gl.getAttribLocation(setupProgram(), 'angle')
         gl.enableVertexAttribArray(loc)
         gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer())
-        gl.vertexAttribPointer(loc, 2, gl.FLOAT, false, 0, 0)
+        gl.vertexAttribPointer(loc, 1, gl.FLOAT, false, 0, 0)
     }
 
     function draw() {
-        updateVertices(new Date().getTime())
-        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.DYNAMIC_DRAW)
-        gl.drawArrays(gl.LINE_LOOP, 0, vertices.length / 2)
+        updateAngles(new Date().getTime())
+        gl.bufferData(gl.ARRAY_BUFFER, angles, gl.DYNAMIC_DRAW)
+        gl.drawArrays(gl.LINE_LOOP, 0, angles.length)
         requestAnimationFrame(draw)
 
-        function updateVertices(millis) {
+        function updateAngles(millis) {
             var d = PI2 / n
             var angle = millis % 20000 * PI2 / 20000
             var i = 0
-            while (i < vertices.length) {
-                vertices[i++] = Math.cos(angle)
-                vertices[i++] = Math.sin(angle)
-                angle += d
-            }
+            while (i < angles.length)
+                angles[i++] = (angle += d)
         }
     }
 }
