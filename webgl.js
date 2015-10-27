6bf0bf5e59147c6c027cd8b2933ec1b53774e2ba
Rudolf Hersén <rudolf@qwaya.com>
2015-10-27 07:52:29 +0100

    draw function of x in interval [-1, 1]

--- webgl.js
+++ webgl.js
@@ -2,17 +2,32 @@
 /* exported webgl */
 
 function webgl() {
-    var PI2 = 2 * Math.PI
     var canvas = document.getElementById('webgl')
     var gl = canvas.getContext('webgl')
-    var n = 5;
-    var angles = new Float32Array(n)
+    var a = []
+    for (var i = 0, x = -1; x <= 1; x += 0.1) {
+        a[i++] = x
+    }
+    var xs = new Float32Array(a)
 
     bind()
 
     gl.viewport(0, 0, canvas.width, canvas.height)
 
     requestAnimationFrame(draw)
+
+    function bind() {
+        var loc = gl.getAttribLocation(setupProgram(), 'x')
+        gl.enableVertexAttribArray(loc)
+        gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer())
+        gl.vertexAttribPointer(loc, 1, gl.FLOAT, false, 0, 0)
+    }
+
+    function draw() {
+        gl.bufferData(gl.ARRAY_BUFFER, xs, gl.DYNAMIC_DRAW)
+        gl.drawArrays(gl.LINE_STRIP, 0, xs.length)
+        requestAnimationFrame(draw)
+    }
 
     function setupProgram() {
         var r = createProgram()
@@ -24,8 +39,8 @@ function webgl() {
         var r = gl.createProgram()
 
         gl.attachShader(r, createShader(gl.VERTEX_SHADER, [
-            'attribute float angle',
-            'void main() { gl_Position = vec4(cos(angle), sin(angle), 0, 1)',
+            'attribute float x',
+            'void main() { gl_Position = vec4(x, sin(x), 0, 1)',
             '}'
         ].join(';')))
 
@@ -45,28 +60,6 @@ function webgl() {
             if (!gl.getShaderParameter(r, gl.COMPILE_STATUS))
                 alert(gl.getShaderInfoLog(r))
             return r
-        }
-    }
-
-    function bind() {
-        var loc = gl.getAttribLocation(setupProgram(), 'angle')
-        gl.enableVertexAttribArray(loc)
-        gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer())
-        gl.vertexAttribPointer(loc, 1, gl.FLOAT, false, 0, 0)
-    }
-
-    function draw() {
-        updateAngles(new Date().getTime())
-        gl.bufferData(gl.ARRAY_BUFFER, angles, gl.DYNAMIC_DRAW)
-        gl.drawArrays(gl.LINE_LOOP, 0, angles.length)
-        requestAnimationFrame(draw)
-
-        function updateAngles(millis) {
-            var d = PI2 / n
-            var angle = millis % 20000 * PI2 / 20000
-            var i = 0
-            while (i < angles.length)
-                angles[i++] = (angle += d)
         }
     }
 }
