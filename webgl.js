d9c011f57aaf1521e8bf05306146a51c6e6a4a92
Rudolf Hersén <rudolf@qwaya.com>
2015-10-27 20:24:33 +0100

    draw x and y axes with separate shaders

--- webgl.js
+++ webgl.js
@@ -4,43 +4,63 @@
 function webgl() {
     var canvas = document.getElementById('webgl')
     var gl = canvas.getContext('webgl')
-    var a = []
-    for (var i = 0, x = -1; x <= 1; x += 0.1) {
-        a[i++] = x
-    }
-    var xs = new Float32Array(a)
+    var xs = new Float32Array(getXs())
+    var axis = new Float32Array([
+        -1, 0, 1, 0,
+        1, 0, 0.95, 0.02,
+        1, 0, 0.95, -0.02,
+        0, -1, 0, 1,
+        0, 1, 0.02, 0.95,
+        0, 1, -0.02, 0.95
+    ])
+    var graph = graphProgram(), axes = axesProgram()
+    var pos
+    var x
 
     bind()
 
-    gl.viewport(0, 0, canvas.width, canvas.height)
-
     requestAnimationFrame(draw)
 
+    function getXs() {
+        var a = []
+        for (var x = -1; x < 1; x += 0.005)
+            a.push(x)
+        a.push(1)
+        return a
+    }
+
     function bind() {
-        var loc = gl.getAttribLocation(setupProgram(), 'x')
-        gl.enableVertexAttribArray(loc)
         gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer())
-        gl.vertexAttribPointer(loc, 1, gl.FLOAT, false, 0, 0)
+
+        x = gl.getAttribLocation(graph, 'x')
+        gl.enableVertexAttribArray(x)
+
+        pos = gl.getAttribLocation(axes, 'pos')
+        gl.enableVertexAttribArray(pos)
     }
 
     function draw() {
+        gl.lineWidth(1)
+        gl.useProgram(axes)
+        gl.vertexAttribPointer(pos, 2, gl.FLOAT, false, 0, 0)
+        gl.bufferData(gl.ARRAY_BUFFER, axis, gl.DYNAMIC_DRAW)
+        gl.drawArrays(gl.LINES, 0, axis.length / 2)
+
+        gl.lineWidth(3)
+        gl.useProgram(graph)
+        gl.vertexAttribPointer(x, 1, gl.FLOAT, false, 0, 0)
         gl.bufferData(gl.ARRAY_BUFFER, xs, gl.DYNAMIC_DRAW)
         gl.drawArrays(gl.LINE_STRIP, 0, xs.length)
+
         requestAnimationFrame(draw)
     }
 
-    function setupProgram() {
-        var r = createProgram()
-        gl.useProgram(r)
-        return r
-    }
-
-    function createProgram() {
+    function graphProgram() {
         var r = gl.createProgram()
 
         gl.attachShader(r, createShader(gl.VERTEX_SHADER, [
             'attribute float x',
-            'void main() { gl_Position = vec4(x, sin(x), 0, 1)',
+            'void main() { gl_Position = vec4(x, abs(x), 0, 1)',
             '}'
         ].join(';')))
 
@@ -52,14 +72,33 @@ function webgl() {
         gl.linkProgram(r)
 
         return r
+    }
 
-        function createShader(type, source) {
-            var r = gl.createShader(type)
-            gl.shaderSource(r, source)
-            gl.compileShader(r)
-            if (!gl.getShaderParameter(r, gl.COMPILE_STATUS))
-                alert(gl.getShaderInfoLog(r))
-            return r
-        }
+    function axesProgram() {
+        var r = gl.createProgram()
+
+        gl.attachShader(r, createShader(gl.VERTEX_SHADER, [
+            'attribute vec2 pos',
+            'void main() { gl_Position = vec4(pos, 0, 1)',
+            '}'
+        ].join(';')))
+
+        gl.attachShader(r, createShader(gl.FRAGMENT_SHADER, [
+            'void main() { gl_FragColor = vec4(0, 0, 0.6, 1)',
+            '}'
+        ].join(';')))
+
+        gl.linkProgram(r)
+
+        return r
+    }
+
+    function createShader(type, source) {
+        var r = gl.createShader(type)
+        gl.shaderSource(r, source)
+        gl.compileShader(r)
+        if (!gl.getShaderParameter(r, gl.COMPILE_STATUS))
+            alert(gl.getShaderInfoLog(r))
+        return r
     }
 }
