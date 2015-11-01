0e5aa663c1243af606829c30fea69ab75074afd7
Rudolf Hersén <rudolf@qwaya.com>
2015-11-01 12:10:48 +0000

    refactor

--- webgl.js
+++ webgl.js
@@ -1,10 +1,9 @@
 /* exported webgl */
 
 function webgl() {
-    var canvas = document.getElementById('webgl')
-    var gl = canvas.getContext('webgl')
-    var xs = new Float32Array(getXs())
-    var axis = new Float32Array([
+    var gl = document.getElementById('webgl').getContext('webgl')
+    var xTypedArray = new Float32Array(getXArray())
+    var axesTypedArray = new Float32Array([
         -1, 0, 1, 0,
         1, 0, 0.95, 0.02,
         1, 0, 0.95, -0.02,
@@ -13,22 +12,22 @@ function webgl() {
         0, 1, -0.02, 0.95
     ])
 
-    var $func = document.getElementById('func');
-    var $shaderError = document.getElementById('shader-error');
+    var shaderErrorElement = document.getElementById('shader-error');
+    var funcElement = document.getElementById('func');
 
-    var graph = graphProgram()
+    var graph = graphProgram(funcElement.value = 'sin(x)')
     var axes = axesProgram();
     var pos = gl.getAttribLocation(axes, 'pos')
 
     var x, t, xsBuffer, axisBuffer
 
-    $func.addEventListener('input', onInput)
+    funcElement.addEventListener('input', onInput)
 
     bind()
 
     requestAnimationFrame(draw)
 
-    function getXs() {
+    function getXArray() {
         var a = []
         for (var x = -1; x < 1; x += 0.005)
             a.push(x)
@@ -38,7 +37,7 @@ function webgl() {
 
     function onInput() {
         gl.deleteProgram(graph)
-        graph = graphProgram()
+        graph = graphProgram(funcElement.value)
         if (graph) {
             t = gl.getUniformLocation(graph, 't')
             x = gl.getAttribLocation(graph, 'x')
@@ -57,41 +56,48 @@ function webgl() {
     }
 
     function draw() {
-        gl.lineWidth(1)
-        gl.useProgram(axes)
-        gl.bindBuffer(gl.ARRAY_BUFFER, axisBuffer)
-        gl.vertexAttribPointer(pos, 2, gl.FLOAT, false, 0, 0)
-        gl.bufferData(gl.ARRAY_BUFFER, axis, gl.DYNAMIC_DRAW)
-        gl.drawArrays(gl.LINES, 0, axis.length / 2)
+        drawAxes();
 
-        if (graph) {
-            gl.lineWidth(3)
-            gl.useProgram(graph)
-            gl.bindBuffer(gl.ARRAY_BUFFER, xsBuffer)
-            gl.vertexAttribPointer(x, 1, gl.FLOAT, false, 0, 0)
-            gl.uniform1f(t, new Date().getTime() % 8000 / 8000)
-            gl.bufferData(gl.ARRAY_BUFFER, xs, gl.DYNAMIC_DRAW)
-            gl.drawArrays(gl.LINE_STRIP, 0, xs.length)
-        }
+        if (graph)
+            drawGraph();
 
         requestAnimationFrame(draw)
     }
 
-    function graphProgram() {
+    function drawAxes() {
+        gl.lineWidth(1)
+        gl.useProgram(axes)
+        gl.bindBuffer(gl.ARRAY_BUFFER, axisBuffer)
+        gl.vertexAttribPointer(pos, 2, gl.FLOAT, false, 0, 0)
+        gl.bufferData(gl.ARRAY_BUFFER, axesTypedArray, gl.DYNAMIC_DRAW)
+        gl.drawArrays(gl.LINES, 0, axesTypedArray.length / 2)
+    }
+
+    function drawGraph() {
+        gl.lineWidth(3)
+        gl.useProgram(graph)
+        gl.bindBuffer(gl.ARRAY_BUFFER, xsBuffer)
+        gl.vertexAttribPointer(x, 1, gl.FLOAT, false, 0, 0)
+        gl.uniform1f(t, new Date().getTime() % 8000 / 8000)
+        gl.bufferData(gl.ARRAY_BUFFER, xTypedArray, gl.DYNAMIC_DRAW)
+        gl.drawArrays(gl.LINE_STRIP, 0, xTypedArray.length)
+    }
+
+    function graphProgram(f) {
         var r = gl.createProgram()
         var shader = createShader(gl.VERTEX_SHADER, [
             'attribute float x',
             'uniform float t',
-            'void main() { gl_Position = vec4(x, ' + $func.value + ', 0, 1)',
+            'void main() { gl_Position = vec4(x, ' + f + ', 0, 1)',
             '}'
         ].join(';'));
 
         if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-            $shaderError.innerHTML = gl.getShaderInfoLog(shader)
+            shaderErrorElement.innerHTML = gl.getShaderInfoLog(shader)
             return
         }
 
-        $shaderError.innerHTML = ''
+        shaderErrorElement.innerHTML = ''
         gl.attachShader(r, shader)
 
         shader = createShader(gl.FRAGMENT_SHADER, [
@@ -99,7 +105,7 @@ function webgl() {
             '}'
         ].join(';'));
         if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
-            $shaderError.innerHTML = gl.getShaderInfoLog(shader)
+            shaderErrorElement.innerHTML = gl.getShaderInfoLog(shader)
         gl.attachShader(r, shader)
 
         gl.linkProgram(r)
@@ -116,7 +122,7 @@ function webgl() {
             '}'
         ].join(';'));
         if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
-            $shaderError.innerHTML = gl.getShaderInfoLog(shader)
+            shaderErrorElement.innerHTML = gl.getShaderInfoLog(shader)
         gl.attachShader(r, shader)
 
         shader = createShader(gl.FRAGMENT_SHADER, [
@@ -124,7 +130,7 @@ function webgl() {
             '}'
         ].join(';'));
         if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
-            $shaderError.innerHTML = gl.getShaderInfoLog(shader)
+            shaderErrorElement.innerHTML = gl.getShaderInfoLog(shader)
         gl.attachShader(r, shader)
 
         gl.linkProgram(r)
